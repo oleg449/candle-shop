8e56bed07e9440773ee69cdea600ae14706700a1
Oleg <supegananas@gmail.com>
2026-07-04 18:04:07 +0300

    Updated site

--- Git_Update.bat
+++ Git_Update.bat
@@ -4,8 +4,13 @@ setlocal
 git add .
 if errorlevel 1 goto error
 
-git commit -m "Updated site"
-if errorlevel 1 goto error
+git diff --cached --quiet
+if errorlevel 1 (
+  git commit -m "Updated site"
+  if errorlevel 1 goto error
+) else (
+  echo No changes to commit.
+)
 
 git push
 if errorlevel 1 goto error
